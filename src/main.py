dc154a041291b92e4fa0f525ad3bfd921bba4b2a
Thomas Kirby <kirbyt1@mcmaster.ca>
2023-05-16 15:26:53 -0400

    Completed new plotting function and start function in main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #/usr/bin/python3
 from cmd import Cmd
+from sys import exit
 import communications #handles all communication related functions.
 import plotting #handles plotting
 import fileHandling #file handling stuff
@@ -13,18 +14,32 @@ class MyPrompt(Cmd):
     file = None
 
     def do_start(self, inp):
-        """    Starts a new process which is used for data acquisition. This continues until exit or stop command is issued..
+        """    
+        Starts a new process which is used for data acquisition. 
+        This continues until exit or stop command is issued. If
+        the data is already being acquired, then it will exit the function.
         """
+        try:
+            #check if data collection process already exist
+            if data_aq_process.is_alive():
 
-        #check if data collection process already exists
-       if p.is_alive():
-            print("Run already started")
-            return
-        else:
+                print("Data acquisition process already running!")
+
+                return
+
+        except NameError: # if NameError exception is raised than process was not defined. aka doesn't exist.
+            # if the data_aq process doesn't exists the function will continue.
+
+        #check if at least one edaq device is connected. get_com_ports returns a list of serial devices. checks if list is empty
+            if not communications.get_com_ports():
+
+                print("No eDAQ device connected.")
+
+                return
+
             # exporting some global variables
-
             global file
-            #file = namefile()
+            file = fileHandling.namefile()
 
             global start_time
             start_time = datetime.now()
@@ -35,18 +50,21 @@ class MyPrompt(Cmd):
             global fieldnames
             fieldnames = ['systime', 'date', 'time_elapsed'] + sensors.split()
 
-            filecreate(file, fieldnames)
+            fileHandling.filecreate(file, fieldnames) # taking above input to generate file.
             
-            p = mp(target="communications.start_acquisition",
+            data_aq_process = mp(target="communications.start_acquisition",
                     args=('file',)
                     ) # creates process to do data aqcquisition
 
-            p.start() # starts process
+            data_aq_process.start() # starts child process
 
         # checking if process was started sucessfully.
-           if p.is_alive():
+            if data_aq_process.is_alive():
+
                 print("Run initiated.")
+
             else:
+
                 print(
                         "The program encountered and error initiating run... \n The program will now exit."
                         )
@@ -57,28 +75,67 @@ class MyPrompt(Cmd):
         """starts plotting data.
         """
 
-        if p.is_a
+        # checks if data_aq_process if running exits if running
+        try:
 
-        if p2.is_alive():
-            print("Run initiated.")
-            response = False
-        else:
-            print("The program encountered and error initiating run... \n The program will now exit.")
-            response = True
+            data_aq_process.is_alive()
 
-        return response
-        
-        return
+            if not data_aq_process.is_alive(): 
+
+                print(
+                        "Data acquisition process not running. Run start to start acquiring data."
+                        )
+
+                return
+            
+            else:
+
+                plotting_process = mp(target="plotting.plot",
+                        args=('file',)
+                        )
+
+                plotting_process.start()
+
+                if plotting_process.is_alive():
+
+                    print("Plotting initiated.")
+
+                else:
+
+                    print("Plotting error.")
+
+                return
+
+        except NameError:
+
+            print("Data acquisition process not running. Run start to start acquiring data.")
+
+            return
+ 
     
     def do_devicelist(self, inp):
+
         print(communications.get_com_ports())
+
         return
 
-    def do_exit(self, inp)
+ 
+
+    def do_exit(self, inp):
+
+        # closing child processes
+
+
+        exit() # exits the program if no other threads are running?
+
+        return
 
     
 
     def do_spawn_shell(self, inp):
+
+        return
+
 
 def main():
     
