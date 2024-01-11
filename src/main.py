d89cd220783ecdc23126667c450b84bb982a0d54
SchrodingersCat6626 <thomas@thomaskirby.net>
2024-01-11 13:48:38 -0500

    added shutdownblock to prevent windows from shutting down while program is running. (To be tested.)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 #/usr/bin/python3
 
 from bapvuPrompt import bapvuPrompt
+from os import name
+from ctypes import *
+
 
 if __name__ == '__main__':
+
+    if name == 'nt':
+        from ctypes import *
+        retval = windll.user32.ShutdownBlockReasonCreate(
+            'BAPVu',
+            c_wchar_p("BAPVu is currently running!")
+            )   
+        if retval != 0:
+                print('test')
     
     bapvuPrompt(completekey='tab', stdin=None, stdout=None).cmdloop()
